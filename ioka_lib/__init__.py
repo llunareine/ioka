8050070353b6cc3a3e6b6d5c51586f0a21f58315
Aiganym <llunareine@gmail.com>
2024-01-25 23:16:16 +0600

    feat: add customers and cards

--- ioka_lib/__init__.py
+++ ioka_lib/__init__.py
@@ -1,5 +1,9 @@
 from orders import Orders
 from payments import Payments
+from customers import Customers
 
 orders = Orders()
 payment = Payments()
+customers = Customers()
+cards = Customers.Cards()
+
