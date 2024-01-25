6e4098e886847e2efd35627ca13203fe6a6a4a7f
Aiganym <llunareine@gmail.com>
2024-01-25 23:20:34 +0600

    feat: add dashboard

--- ioka_lib/__init__.py
+++ ioka_lib/__init__.py
@@ -2,11 +2,13 @@ from orders import Orders
 from payments import Payments
 from customers import Customers
 from webhooks import Webhooks
+from dashboard import Dashboard
 
 orders = Orders()
 payment = Payments()
 customers = Customers()
 cards = Customers.Cards()
 webhooks = Webhooks()
+dashboard = Dashboard()
 
 
