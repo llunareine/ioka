911e87d037754d6d2d3b91ce6493681abfda7905
Aiganym <llunareine@gmail.com>
2024-01-26 03:04:56 +0600

    feat: dynamic restructure of response

--- ioka_lib/base.py
+++ ioka_lib/base.py
@@ -5,10 +5,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+class ConvertToObj:
+    def init(self, **entries):
+        self.dict.update(entries)
+
+    def repr(self):
+        attributes = ', '.join(f"{key}={value!r}" for key, value in self.dict.items())
+        return f"{attributes}"
+    def str(self):
+        attributes = ', '.join(f"{key}: {value}" for key, value in self.dict.items())
+        return f"{attributes}"
+
+class Response:
+    def init(self, data):
+        if isinstance(data, list):
+            self.data = [ConvertToObj(**item) for item in data]
+        else:
+            self.data = ConvertToObj(**data)
+
+
 class BaseAPI:
 
-    def __init__(self, api_key):
-        self.base_url = os.getenv("BASE_URL")
+    def init(self, api_key):
+        self.base_url = "https://stage-api.ioka.kz/v2"
         self.api_key = api_key
 
     def _send_request(self, method, endpoint, data=None):
@@ -18,7 +37,12 @@ class BaseAPI:
             "Content-Type": "application/json"
         }
         response = requests.request(method, url, headers=headers, json=data)
+        if method == 'DELETE' and response.status_code == 204:
+            return 'Successfully deleted'
+        if 400 <= response.status_code < 600:
+            return response.json()['message']
 
-        print(response)
-
-        return response.status_code, response.json()
+        if isinstance(response.json(), list):
+            return Response(response.json())
+        else:
+            return ConvertToObj(**response.json())
