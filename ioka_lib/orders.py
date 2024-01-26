9cdfb976e19c74d5be0144ab7414c99777f158c7
Aiganym <llunareine@gmail.com>
2024-01-26 15:39:15 +0600

    feat: optimize orders

--- ioka_lib/orders.py
+++ ioka_lib/orders.py
@@ -4,6 +4,22 @@ load_dotenv()
 
 class Orders(BaseAPI):
 
+    def _validate_currency(self, currency):
+        if not isinstance(currency, str) or len(currency) != 3:
+            raise ValueError("Currency must be a 3-letter string")
+
+    def _validate_amount(self, amount):
+        if not isinstance(amount, (int, float)) or amount < 100:
+            raise ValueError("Amount must be >= 100")
+
+    def _validate_reason(self, reason):
+        if not isinstance(reason, str) or len(reason) > 255:
+            raise ValueError("Reason must be a string of 255 characters or less")
+
+    def _validate_capture_method(self, capture_method):
+        if capture_method not in ['AUTO', 'MANUAL']:
+            raise ValueError("Capture method must be 'AUTO' or 'MANUAL'")
+
     def get_orders(self):
         return self._send_request("GET", "orders")
 
@@ -20,43 +36,21 @@ class Orders(BaseAPI):
         return self._send_request("GET", f"orders/{order_id}/refunds/{refund_id}")
 
     def create_order(self, amount, currency, capture_method):
-        if not isinstance(amount, (int, float)) or amount < 100:
-            raise ValueError("Amount must be >= 100")
-        if not isinstance(currency, str) or len(currency) != 3:
-            raise ValueError("Currency must be a 3-letter string")
-        if capture_method not in ['AUTO', 'MANUAL']:
-            raise ValueError("Capture method must be 'manual' or 'automatic'")
+        self._validate_amount(amount)
+        self._validate_currency(currency)
+        self._validate_capture_method(capture_method)
 
-        order_data = {
-            "amount": amount,
-            "currency": currency,
-            "capture_method": capture_method
-        }
+        order_data = {"amount": amount, "currency": currency, "capture_method": capture_method}
         return self._send_request("POST", "orders", data=order_data)
 
     def capture_order(self, order_id, amount):
-        if not isinstance(amount, (int, float)) or amount < 100:
-            raise ValueError("Amount must be >= 100")
-        order_data = {
-            "amount": amount,
-
-        }
-        return self._send_request("POST", f"orders/{order_id}/capture", data=order_data)
+        self._validate_amount(amount)
+        return self._send_request("POST", f"orders/{order_id}/capture", data={"amount": amount})
 
     def refund_order(self, order_id, amount):
-        if not isinstance(amount, (int, float)) or amount < 100:
-            raise ValueError("Amount must be >= 100")
-        order_data = {
-            "amount": amount
-        }
-        return self._send_request("POST", f"orders/{order_id}/refunds", data=order_data)
+        self._validate_amount(amount)
+        return self._send_request("POST", f"orders/{order_id}/refunds", data={"amount": amount})
 
     def cancel_order(self, order_id, reason):
-        if not isinstance(reason, str) or len(reason) > 255:
-            raise ValueError("Reason must be a string of 255 characters or less")
-
-        data = {
-            "reason": reason
-        }
-        return self._send_request("POST", f"orders/{order_id}/cancel", data=data)
-
+        self._validate_reason(reason)
+        return self._send_request("POST", f"orders/{order_id}/cancel", data={"reason": reason})
